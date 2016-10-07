da7ec88c5ad4078eec6f0e37639b78a54daf1fbb
rs <senthil@sangamsoftech.com>
2016-10-07 19:21:47 +0530

    category test

--- db/migrate/20160514081422_create_categories.rb
+++ db/migrate/20160514081422_create_categories.rb
@@ -2,11 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories, id: :uuid do |t|
       t.string :name
-      t.integer :code
-      t.references :parent
-			t.boolean :shared
-			t.belongs_to  :user, index: true
-
+      t.uuid :parent
 			t.datetime :deleted, index: true
 			t.integer :lock_version, default: 0, null: false
 
