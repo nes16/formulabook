5516cba05af8debba32a8fce7e466d4519837926
rs <senthil@sangamsoftech.com>
2016-11-02 14:44:52 +0530

    mv profile.d

---
+++ client/formulabookapp/typings/cordova/plugins/StatusBar-tests.js
@@ -0,0 +1,14 @@
+// Licensed under the MIT license.
+/// <reference path="StatusBar.d.ts" />
+var statusBar = window.StatusBar;
+statusBar.overlaysWebView(true);
+statusBar.overlaysWebView(false);
+statusBar.styleDefault();
+statusBar.styleLightContent();
+statusBar.styleBlackTranslucent();
+statusBar.styleBlackOpaque();
+statusBar.backgroundColorByName("red");
+statusBar.backgroundColorByHexString("#aaa");
+statusBar.hide();
+statusBar.show();
+var visible = statusBar.isVisible;
