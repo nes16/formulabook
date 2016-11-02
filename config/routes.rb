2d2e375f5d11d05a71793c1c571ce64b0d649e72
rs <senthil@sangamsoftech.com>
2016-11-02 16:31:47 +0530

    set ln to public folder

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'formulabook', :to => redirect('/formulabook/index.html')
+  #get 'formulabook/index.html', :to => redirect('/formulabook/index.html')
 
   namespace :api do
     namespace :v1 do
