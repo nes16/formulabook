2292b532ba1dfc0aae177453e8b837db92653fec
rs <senthil@sangamsoftech.com>
2016-12-03 10:54:42 +0530

    ngrx

--- client/formulabookapp/src/components/formula/run/run.ts
+++ client/formulabookapp/src/components/formula/run/run.ts
@@ -9,16 +9,16 @@ import { Observable } from 'rxjs/Observable';
 	templateUrl: 'val.html',
 })
 
-export class ValComponent{
+export class ValComponent {
 	constructor(nds: NewDataService) {
 
 	}
 	@Input() resource;
-	@Input() editMode:boolean = false;
-	
+	@Input() editMode: boolean = false;
+
 
 	ngOnInit() {
-		
+
 	}
 
 }
@@ -28,48 +28,45 @@ export class ValComponent{
 	selector: 'fl-run',
 	templateUrl: 'run.html',
 })
-export class RunComponent{
-	form:FormGroup;
-    editModein:boolean = false;
-	valueChange$:Observable<any>;
+export class RunComponent {
+	form: FormGroup;
+	editModein: boolean = false;
+	valueChange$: Observable<any>;
 	constructor(public nds: NewDataService) {
 
 	}
-	@Input() resource:FormulaRun;
-	@Input() formula:Formula;
+	@Input() resource: FormulaRun;
+	@Input() formula: Formula;
 	ngOnInit() {
-			let r = this.resource;
-			this.form = new FormGroup({
-				name: new FormControl(r.name, [ Validators.minLength(2)
-											, Validators.maxLength(30)]),
-                value: new FormControl(r.result)
-            })
-            let keys = Object.keys(r.values);
-			let vals$:Observable<any>[] = [];
-			keys.forEach((k,i) => {
-				let fc = new FormControl(r.values[k], [Validators.required]) as FormControl;
-				this.form.addControl("Var"+i, fc );
-				vals$.push(fc.valueChanges);
-            })
-			this.valueChange$ = Observable.from(vals$)
+		let r = this.resource;
+		this.form = new FormGroup({
+			name: new FormControl(r.name, [Validators.minLength(2)
+				, Validators.maxLength(30)]),
+			value: new FormControl(r.result)
+		})
+		let keys = Object.keys(r.values);
+		let vals$: Observable<any>[] = [];
+		keys.forEach((k, i) => {
+			let fc = new FormControl(r.values[k], [Validators.required]) as FormControl;
+			this.form.addControl("Var" + i, fc);
+			vals$.push(fc.valueChanges);
+		})
+		this.valueChange$ = Observable.from(vals$)
 			.map(i => i)
 			.combineAll(r => {
 				this.nds.evaluateFormula(this.resource, this.formula);
 			})
-
-			
-
 	}
 
-    getKeys(){
-        return Object.keys(this.resource.values)
-    }
+	getKeys() {
+		return Object.keys(this.resource.values)
+	}
 
-    onPress(evt) {
-	// 	let runRow = evt.target;
-    console.log('Long press row')
-         this.editModein = !this.editModein;
-    }
+	onPress(evt) {
+		// 	let runRow = evt.target;
+		console.log('Long press row')
+		this.editModein = !this.editModein;
+	}
 
-    
+
 }
